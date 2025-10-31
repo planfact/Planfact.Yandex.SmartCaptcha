f73fab948959cc150629666da9e88b0fd59d612b
Юрий Дубровских <jurahf@gmail.com>
2025-10-31 18:57:30 +0500

    Проверка наличия токена капчи производится после проверки отключения капчи (#1)
    Валидация токена перенесена таким образом, чтобы она выполнялась после проверки настроек валидатора и IP whitelist. Ранее передача пустой строки токена в метод валидации вызывала ошибку даже при отключенном валидаторе и исключенном IP
    
    ---------
    
    Co-authored-by: Юрий Дубровских <u.dubrovskikh@planfact.io>

--- tests/Planfact.Yandex.SmartCaptcha.Tests/SmartCaptchaValidatorTests.cs
+++ tests/Planfact.Yandex.SmartCaptcha.Tests/SmartCaptchaValidatorTests.cs
@@ -17,27 +17,33 @@ public class SmartCaptchaValidatorTests : SmartCaptchaTestsBase
             .WithParameterName("token");
     }
 
-    [Fact]
-    public async Task ValidateAsync_WithDisabledValidation_ReturnsSuccess()
+    [Theory]
+    [InlineData(TestConstants.DefaultToken)]
+    [InlineData("")]
+    [InlineData("  ")]
+    public async Task ValidateAsync_WithDisabledValidation_ReturnsSuccess(string token)
     {
         // Arrange
         SmartCaptchaValidator validator = CreateValidator(CreateDisabledSettings());
 
         // Act
-        SmartCaptchaValidationResult result = await validator.ValidateAsync(TestConstants.DefaultToken);
+        SmartCaptchaValidationResult result = await validator.ValidateAsync(token);
 
         // Assert
         result.IsValid.Should().BeTrue();
     }
 
-    [Fact]
-    public async Task ValidateAsync_WithSkippedIp_ReturnsSuccess()
+    [Theory]
+    [InlineData(TestConstants.DefaultToken)]
+    [InlineData("")]
+    [InlineData("  ")]
+    public async Task ValidateAsync_WithSkippedIp_ReturnsSuccess(string token)
     {
         // Arrange
         SmartCaptchaValidator validator = CreateValidator(CreateSettingsWithSkipIps(TestConstants.DefaultIp));
 
         // Act
-        SmartCaptchaValidationResult result = await validator.ValidateAsync(TestConstants.DefaultToken, TestConstants.DefaultIp);
+        SmartCaptchaValidationResult result = await validator.ValidateAsync(token, TestConstants.DefaultIp);
 
         // Assert
         result.IsValid.Should().BeTrue();
